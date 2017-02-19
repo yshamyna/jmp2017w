6a9292c481bbcb866c64ff5b8f40bc915e4d7e80
Yury Shamyna <Yury_Shamyna@epam.com>
2017-02-19 20:47:04 +0200

    Module 02, task 1: fixed java compile command

--- 02-Troubleshooting/Task1/nonmemoryleak/create-jar.bat
+++ 02-Troubleshooting/Task1/nonmemoryleak/create-jar.bat
@@ -1,4 +1,4 @@
 mkdir target
-avac -classpath ./target -d ./target src/com/jmp2017w/nonmemoryleak/tasks/*.java src/com/jmp2017w/nonmemoryleak/Runner.java
+javac -classpath ./target -d ./target src/com/jmp2017w/nonmemoryleak/tasks/*.java src/com/jmp2017w/nonmemoryleak/Runner.java
 xcopy /y resources\* target
 jar cvmf META-INF/manifest.mf nonmemoryleak.jar -C ./target .
